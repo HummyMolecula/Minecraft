e80b62bb7bab2c80fed737bd1468bbc978dcc759
HummyMolecula <dragonitdeveloper@gmail.com>
2022-02-26 14:16:03 +0200

    now you can create block!

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ class Voxel(Button):
             highlight_color = color.lime
         )
 
+    def input(self, key):
+        if self.hovered:
+            if key == "left mouse down":
+                voxel = Voxel(position = self.position + mouse.normal)
+
 app = Ursina()
 
 for z in range(20):
