523a0d42166edc9056383d56ca57b5c92b2ca928
HummyMolecula <dragonitdeveloper@gmail.com>
2022-02-26 14:00:21 +0200

    create single button

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from ursina import *
 
+class Voxel(Button):
+    def __init__(self):
+        super.__init__(
+            parent = scene,
+            position = (0, 0, 0),
+            model = "cube",
+            origin_y = 0.5,
+            texture = "white_cube",
+            color = color.white,
+            highlight_color = color.lime
+        )
+
 app = Ursina()
 
-test_square = Entity(model="circle", color=color.red)
+voxel = Voxel()
 
 app.run()
