828148c88354f60b159961731f369dc743e171c3
HummyMolecula <dragonitdeveloper@gmail.com>
2022-02-26 15:17:54 +0200

    added sky

--- main.py
+++ main.py
@@ -7,18 +7,19 @@ grass_texture = load_texture("assets/grass_block.png")
 stone_texture = load_texture("assets/stone_block.png")
 brick_texture = load_texture("assets/brick_block.png")
 dirt_texture = load_texture("assets/dirt_block.png")
+sky_texture = load_texture("assets/skybox.png")
 block_pick = 1
 
 def update():
     global block_pick
 
-    if held_keys[1]:
+    if held_keys["1"]:
         block_pick = 1
-    elif held_keys[2]:
+    elif held_keys["2"]:
         block_pick = 2
-    elif held_keys[3]:
+    elif held_keys["3"]:
         block_pick = 3
-    elif held_keys[4]:
+    elif held_keys["4"]:
         block_pick = 4
 
 class Voxel(Button):
@@ -48,11 +49,22 @@ class Voxel(Button):
 
             if key == "right mouse down":
                 destroy(self)
- 
+
+class Sky(Entity):
+    def __init__(self):
+        super().__init__(
+            parent = scene,
+            model = "sphere",
+            texture = sky_texture,
+            scale = 150,
+            double_sided = True
+        )
+
 for z in range(20):
     for x in range(20):
         voxel = Voxel((x, 0, z))
 
 player = FirstPersonController()
+sky = Sky()
 
 app.run()
